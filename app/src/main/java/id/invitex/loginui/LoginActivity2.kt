439e34cd9183afcc1515b0494d05a2e7ebaa993c
Sandi Setiawan <sandisetiawan444@gmail.com>
2019-03-03 01:01:05 +0700

    add login ui v2

---
+++ app/src/main/java/id/invitex/loginui/LoginActivity2.kt
@@ -0,0 +1,62 @@
+package id.invitex.loginui
+
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login2.*
+
+/**
+ * A login screen that offers login via email/password.
+ */
+class LoginActivity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login2)
+
+        email_sign_in_button.setOnClickListener { attemptLogin() }
+    }
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private fun attemptLogin() {
+        // Reset errors.
+        email.error = null
+
+        // Store values at the time of the login attempt.
+        val emailStr = email.text.toString()
+        val passwordStr = password.text.toString()
+
+        var cancel = false
+        var focusView: View? = null
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(emailStr)) {
+            email.error = getString(R.string.error_field_required)
+            focusView = email
+            cancel = true
+        } else if (!isEmailValid(emailStr)) {
+            email.error = getString(R.string.error_invalid_email)
+            focusView = email
+            cancel = true
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView?.requestFocus()
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+
+        }
+    }
+
+    private fun isEmailValid(email: String): Boolean {
+        return email.contains("@")
+    }
+}
